9eacd5cb6dacf206ad1bd4c59601d9b3d65cd5b8
Dempsey Zhang <zsg258852@gmail.com>
2025-04-15 11:22:23 -0400

    fixed build error

--- auth.ts
+++ auth.ts
@@ -1,6 +1,11 @@
-import Google from "@auth/core/providers/google";
 import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
 });

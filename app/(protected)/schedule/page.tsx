f3034c71e159e7081d2747af9873d777108567e9
kunlong.li <kunlong.li@outlook.com>
2025-04-18 19:33:49 -0400

    fix type

--- app/(protected)/schedule/page.tsx
+++ app/(protected)/schedule/page.tsx
@@ -1,18 +1,22 @@
 import { fetchAllGamesFromDb, fetchGames } from "@/actions/games";
 
 import AllGamesSection from "@/components/Games/AllGamesSection";
+import { Game } from "@/types/IGames";
+import { NBAGame } from "@balldontlie/sdk";
 
 export default async function PredictionPage() {
-  const allGames = await fetchGames();
+  const allGames = await fetchGames() ;
   const dbGames = await fetchAllGamesFromDb();
   const roundMap = Object.fromEntries(
-    dbGames.map((g) => [String(g.apiGameId), g.round?.name ?? null]),
+    dbGames.map((g) => [g.apiGameId, g.round?.name ?? null]),
   );
 
-  const gamesWithRound = allGames.data.map((game) => ({
+  const gamesWithRound: Game[] = (allGames.data as (NBAGame & { datetime: string })[]).map((game) => ({
     ...game,
-    round: roundMap[String(game.id)] ?? null,
+    datetime: game.datetime,
+    round: roundMap[game.id] ?? "",
   }));
+
   return (
     <main className="mx-auto max-w-4xl space-y-10 px-4 py-8">
       <AllGamesSection games={gamesWithRound} isPrediction={false} />
